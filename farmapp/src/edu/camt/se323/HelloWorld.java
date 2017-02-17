eebebdf760ea51a1a9c68b30984e74d610fe731a
pariyathida <pariyathida@gmail.com>
2017-02-17 09:23:33 +0700

    change the package name

---
+++ farmapp/src/edu/camt/se323/HelloWorld.java
@@ -0,0 +1,11 @@
+package edu.camt.se323;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello World");
+	}
+
+}
